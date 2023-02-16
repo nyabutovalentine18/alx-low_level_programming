f2291928d1a23d46a457594bb0aca1363367779e
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-02-16 13:18:33 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A code that displays a new line output using the printf command
+ * Return: (0) (terminated)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
