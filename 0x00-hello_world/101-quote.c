53b39793ed081a5351cc420939f1abe44b746353
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-02-17 00:39:18 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
- * main - A program that prints a line to the standard error
+ * main - Entry Point
+ * prints a line to the standard error using function write
  * Return 1 (Success)
  */
+
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
