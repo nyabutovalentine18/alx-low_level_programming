c3800a698de6ad6ed04023056dc7626c4cedb469
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-02-17 12:39:56 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,31 +9,27 @@
 
 int main(void)
 {
-int a;
-int b;
-int c;
+	int a;
+	int b;
+	int c;
 
-for (a = '0'; a < '9'; a++)
-{
-
-for (b = '0'; b <= '9'; b++)
-{
-
-for (c = b + 1; b <= '9'; b++)
-{
-if ((b != a) != c)
-{
-putchar(a);
-putchar(b);
-putchar(c);
-if (a == '7' && b == '8')
-continue;
-putchar(',');
-putchar(' ');
-}
-}
-}
-}
-putchar('\n');
-return (0);
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 1; b < 10; b++)
+		{
+			for (c = 1; c < 10; c++)
+			{
+				if (a < b && b < c)
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(c + '0');
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
