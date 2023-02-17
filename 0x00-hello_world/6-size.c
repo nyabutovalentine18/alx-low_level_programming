0c76647f7d5a2d6ff7da2855504fca96624f2e21
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-02-17 06:00:39 -0800

    102-print_comb5.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ long int c;
 long long int d;
 float f;
 
-printf("SIZE of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("SIZE of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("SIZE of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("SIZE of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("SIZE of a float: %lu byte(s", (unsigned long)sizeof(f));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)Sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)Sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)Sizeof(c));
+printf("Size of long long int: %lu byte(s)\n", (unsigned long)Sizeof(d));
+printf("Size of a float: %lu byte(s", (unsigned long)Sizeof(f));
 return (0);
 }
