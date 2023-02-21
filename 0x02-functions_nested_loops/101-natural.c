af48851a8e53a432cd6b19abdfb18cc2008772ea
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-02-21 12:19:41 -0800

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - entry point
+*Return: 0
+*/
+
+
+int main(void)
+{
+int a;
+int sum;
+sum = 0;
+
+for (a = 0; a < 10; a++)
+{
+if ((a % 3 == 0) || (a % 5 == 0))
+{
+sum = sum + a;
+}
+}
+printf("%d\n", sum);
+return (0);
+}
+
