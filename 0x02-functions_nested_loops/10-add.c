488dca9b1b674ddcd8649564d1d62d82f5b2e30e
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-02-21 06:14:39 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*add - adds two integers and returns the result
+*@num1: Fisrt parameter
+*@num2: second parameter
+*Return: Integer value
+*/
+
+int add(int num1, int num2)
+{
+return (num1 + num2);
+}
