a852e4333673bd275eff0414486533b39fee39da
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-02-22 23:24:27 -0800

    2-largest_number.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*positive_or_negative - checks if the number is positive or negative
+*@i: Parameter to be checked
+*Return: 0
+*/
+void positive_or_negative(int i)
+{
+if (i < 0)
+printf("%d is negative\n", i);
+else
+printf("%d is positive\n", i);
+}
