40907c26840db10e08033b2e82a0093dcca38ffd
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-02-23 10:06:47 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*mul - multiplies two integers
+*@a: parameter to be checked
+*@b: parameter to be checked
+*Return: the multiplication result
+*/
+
+int mul(int a, int b)
+{
+
+return (a * b);
+}
+
