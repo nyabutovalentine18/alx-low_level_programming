458790cf35f3e87505c73be77d61a4ecc91b4425
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-02-27 04:33:35 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*_puts - prints a string
+*followed by a new line
+*@str: The string to print
+*Return: void
+*/
+
+void _puts(char *str)
+{
+for (; *str != '\0'; str++)
+{
+putchar(*str);
+}
+putchar('\n');
+}
