89cf97b88bc7eed45f309efc0744e7c8bd906c5c
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-03-01 12:15:40 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,7 @@ i = 0;
 
 while (str[i])
 {
-while (!str[i] >= 'a' && str[i] <= 'z')
+while (!(str[i] >= 'a' && str[i] <= 'z'))
 i++;
 if (str[i - 1] == ' ' ||
 str[i - 1] == '\t' ||
@@ -31,7 +31,7 @@ str[i - 1] == '{' ||
 str[i - 1] == '}' ||
 i == 0)
 str[i] -= 32;
-i++
+i++;
 }
 return (str);
 }
