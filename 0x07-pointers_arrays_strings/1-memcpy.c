bc8d1e4c97717a2ad390a8fb275a00023ebf94cf
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-03-06 01:21:29 -0800

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,28 +1,31 @@
 #include "main.h"
 
 /**
-**_memcpy - a function that copies memory area
+**_memcpy - a function that copies memory area.
 *@dest: Destination
 *@src: Source
-*@n: Number of bytes
+*@n: Number of integers
 *Return: dest
 */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int a;
-unsigned int b;
+int a;
+int b;
+
 a = 0;
 while (dest[a] != '\0')
 {
-a++;
+a++
+}
 b = 0;
 while (src[b] != '\0')
 {
 dest[a] = src[b];
 a++;
-b++;
+b++
 }
 dest[a] = '\0';
 return (dest);
 }
+
