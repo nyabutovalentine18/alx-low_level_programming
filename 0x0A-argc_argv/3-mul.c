004845b605849dd2438ab39d45d2c0e1504af7f5
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-03-10 12:05:36 -0800

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,7 +10,7 @@
 */
 int main(int argc, char *argv[])
 {
-int result, num1, num2;
+int num1, num2;
 
 if (argc != 3)
 {
@@ -19,8 +19,7 @@ return (1);
 }
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
-result = num1 * num2;
 
-printf("%d\n", result);
+printf("%d\n", num1 *num2);
 return (0);
 }
