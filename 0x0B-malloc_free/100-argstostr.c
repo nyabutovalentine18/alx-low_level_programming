9f1ddb1caaa30622b34e964509d27341b73c7140
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-03-14 11:39:58 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,48 +2,47 @@
 #include <stdlib.h>
 
 /**
-*argstostr - concatenates all the arguments of your program
-*@ac: argument count
-*@av: argument vector
+* argstostr - Concatenates all the arguments of your program.
+* @ac: Number of arguments
+* @av: Arguments
 *
-*Return: ..
+* Return: Pointer to the new string.
 */
-
 char *argstostr(int ac, char **av)
 {
-int ch = 0;
-int i = 0, j = 0, k = 0;
-char *s;
+int i = 0;
+int j = 0;
+int k = 0;
+int size = 0;
+char *p;
 
 if (ac == 0 || av == NULL)
 return (NULL);
 
-while (i < ac)
+for (i = 0; i < ac; i++)
 {
-while (av[i][j])
-{
-ch++;
-j++;
+for (j = 0; *(*(av + i) + j) != '\0'; j++)
+size++;
+size++;
 }
-j = 0;
-i++;
-}
-s = malloc((sizeof(char) * ch) +ac + 1);
-i = 0;
-while (av[i])
+size++;
+
+p = malloc(sizeof(char) * size);
+if (p == NULL)
+return (NULL);
+
+for (i = 0; i < ac; i++)
 {
-while (av[i][j])
+for (j = 0; *(*(av + i) + j) != '\0'; j++)
 {
-s[k] = av[i][j];
+*(p + k) = *(*(av + i) + j);
 k++;
-j++;
 }
-s[k] = '\n';
-j = 0;
+*(p + k) = '\n';
 k++;
-j++;
 }
 k++;
-s[k] = '\0';
-return (s);
+*(p + k) = '\0';
+
+return (p);
 }
