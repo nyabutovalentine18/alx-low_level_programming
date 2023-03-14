eb5c8ff355877a4fb6a02b039029d296fa134aec
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-03-13 23:31:29 -0700

    3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+* alloc_grid - alloc grid
+* @width: width
+* @height: height
+* Return: array multidimensional
+*/
+
+int **alloc_grid(int width, int height)
+{
+int i, c;
+int **s;
+
+if (width <= 0 || height <= 0)
+return (NULL);
+
+s = malloc(height * sizeof(*s));
+if (s == NULL)
+return (NULL);
+
+for (i = 0; i < height; i++)
+{
+s[i] = malloc(width * sizeof(i));
+if (s[i] == NULL)
+{
+free_grid(s, i);
+return (NULL);
+}
+for (c = 0; c < width; c++)
+s[i][c] = 0;
+}
+return (s);
+}
