34387803e565952746ff01f197e5d8d1d789d1cc
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-03-20 04:26:40 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef STRUCT_H
+#define STRUCT_H
+
+/**
+*@name: First element
+*@age: second element
+*@owner: third element
+*
+*Description: longer description
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+}
+#endif /*dog*/
