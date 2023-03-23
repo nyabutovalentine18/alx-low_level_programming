2f608a88fd5eece8e076eea8878ff74a797317f7
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-03-23 03:54:36 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,12 +8,12 @@
 */
 void print_all(const char * const format, ...)
 {
-va_list valist;
+va_list panything;
 unsigned int i = 0, j, k = 0;
 char *str;
 const char t_arg[] = "cifs";
 
-va_start(valist, format);
+va_start(panything, format);
 while (format && format[i])
 {
 j = 0;
@@ -28,16 +28,16 @@ break;
 switch (format[i])
 {
 case 'c':
-printf("%c", va_arg(valist, int)), k = 1;
+printf("%c", va_arg(panything, int)), k = 1;
 break;
 case 'i':
-printf("%d", va_arg(valist, int)), k = 1;
+printf("%d", va_arg(panything, int)), k = 1;
 break;
 case 'f':
-printf("%f", va_arg(valist, double)), k = 1;
+printf("%f", va_arg(panything, double)), k = 1;
 break;
 case 's':
-str = va_arg(valist, char *), k = 1;
+str = va_arg(panything, char *), k = 1;
 if (!str)
 {
 printf("(nil)");
@@ -47,5 +47,5 @@ printf("%s", str);
 break;
 } i++;
 }
-printf("\n"), va_end(valist);
+printf("\n"), va_end(panything);
 }
