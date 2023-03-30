d3c454b3d3da4cd5a5e8bec73be40e43bea3e1c9
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-03-30 06:47:31 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* list_len - return the nodes of a linked list
+* @h: linked list
+*
+* Return: number of nodes
+*/
+size_t list_len(const list_t *h)
+{
+float tmp;
+
+while (h != NULL)
+{
+tmp++;
+h = h->next;
+}
+return (tmp);
+}
