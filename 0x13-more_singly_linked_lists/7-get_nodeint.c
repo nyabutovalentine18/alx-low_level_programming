7b9cdc92833f8a497394fe17168b0ce63670a821
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-04-04 00:22:52 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,11 +14,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 unsigned int j;
 j = 0;
 listint_t *temp;
-temp = head;
-while (temp && i < index)
+*temp = head;
+while (temp < index && j < index)
 {
 temp = temp->next;
-i++;
+j++;
 }
 
 return (temp ? temp : NULL);
