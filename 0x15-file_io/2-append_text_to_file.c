894e615cfc599843603f1a2f7758c084a1f5c051
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-04-07 12:15:35 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,9 +12,9 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 int o, w;
-int k;
+int k = 0;
 
-if (!filename)
+if (filename == NULL)
 return (-1);
 
 if (text_content != NULL)
