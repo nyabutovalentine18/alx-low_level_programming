92848670b29b300249900c0d5392303a41a5961a
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-04-10 01:54:00 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,10 +12,8 @@ unsigned int binary_to_uint(const char *b)
 unsigned int temp = 0;
 int k = 0;
 
-if (b == NULL)
+if (b != NULL)
 {
-return (0);
-}
 for (; b[k]; k++)
 {
 if (b[k] < '0' || b[k] > '1')
@@ -25,4 +23,7 @@ return (0);
 temp = 2 * temp + (b[k] - '0');
 }
 return (temp);
+}
+else
+return (0);
 }
