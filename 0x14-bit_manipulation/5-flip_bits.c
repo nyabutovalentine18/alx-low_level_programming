cc3ce9546b25e8fe2b3d4abcbe8560f7eb7b5615
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-04-10 01:37:43 -0700

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -2,8 +2,7 @@
 
 
 /**
-* flip_bits -
-* returns the number of bits you would need
+* flip_bits - returns the number of bits you would need
 *to flip to get from one number to another.
 * @n: first element
 * @m: second element
