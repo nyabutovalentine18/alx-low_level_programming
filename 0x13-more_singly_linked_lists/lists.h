ce6e949ab7ec4709ed593cf48336adb1410df53f
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-04-10 12:45:36 -0700

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,6 +18,7 @@ int n;
 struct listint_s *next;
 } listint_t;
 
+size_t free_listint_safe(listint_t **h);
 size_t print_listint_safe(const listint_t *head);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint(const listint_t *h);
