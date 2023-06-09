917fef1391c7fdee3354c4eac8a838c5e216ee1c
nyabutovalentine18 <nyabutovalentine18@gmail.com>
2023-06-09 12:57:11 -0700

    hash tables

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,9 +1,8 @@
 #include "hash_tables.h"
 /**
- * hash_djb2 - implementation of the djb2 algorithm
- * @str: string used to generate hash value
- *
- * Return: hash value
+ *hash_djb2 - implementation for the djb2 algorithm
+ *@str: string to be searched and placed
+ *Return: hash
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
@@ -13,7 +12,7 @@ int c;
 hash = 5381;
 while ((c = *str++))
 {
-hash = ((hash << 5) + hash) +c; /* hash * 33 + c */
+hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 }
 return (hash);
 }
